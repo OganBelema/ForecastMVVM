99c911e9a138c8df4606fb66334c6cb382991049
Belema Ogan <Oganbelema@gmail.com>
2019-04-18 15:48:12 +0100

    Added the fragments and their views for the different navigation menu options Added navigation controller and setup the action bar with the navigation controller In the mobile_navigation file, I mapped the navigation of the different fragments

---
+++ app/src/main/java/com/oganbelema/forecastmvvm/ui/settings/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package com.oganbelema.forecastmvvm.ui.settings
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment: PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+
+    }
+}
