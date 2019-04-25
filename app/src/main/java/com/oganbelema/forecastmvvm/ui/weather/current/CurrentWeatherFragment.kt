4c2b03ae5b50286c0e034864eb4f196d69860b20
Belema Ogan <Oganbelema@gmail.com>
2019-04-25 15:59:44 +0100

    Create Current Weather UI - Add the required views to the xml layout - Bind the current weather data to the view - Create helper functions to bind each data - Update the glide version in gradle - Create GlideModule - Load image to image view using glide

--- app/src/main/java/com/oganbelema/forecastmvvm/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/oganbelema/forecastmvvm/ui/weather/current/CurrentWeatherFragment.kt
@@ -6,12 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 
 import com.oganbelema.forecastmvvm.R
 import com.oganbelema.forecastmvvm.data.network.ApixuWeatherApiService
 import com.oganbelema.forecastmvvm.data.network.ConnectivityInterceptorImpl
 import com.oganbelema.forecastmvvm.data.network.WeatherNetworkDataSourceImpl
+import com.oganbelema.forecastmvvm.internal.glide.GlideApp
 import com.oganbelema.forecastmvvm.ui.base.ScopedFragment
 import kotlinx.android.synthetic.main.current_weather_fragment.*
 import kotlinx.coroutines.Dispatchers
@@ -44,8 +46,56 @@ class CurrentWeatherFragment : ScopedFragment(), KodeinAware {
         val currentWeather = viewModel.weather.await()
         currentWeather.observe(this@CurrentWeatherFragment, Observer {
             if (it != null){
-                textView.text = it.toString()
+                group_loading.visibility = View.GONE
+                updateLocation("Los Angeles")
+                updateDateToToday()
+                updateTemperatures(it.temperature, it.feelsLikeTemperature)
+                updateCondition(it.conditionText)
+                updatePrecipitation(it.precipitationVolume)
+                updateWind(it.windDirection, it.windSpeed)
+                updateVisibility(it.visibilityDistance)
+
+                GlideApp.with(this@CurrentWeatherFragment)
+                    .load("http:${it.conditionIconUrl}")
+                    .into(imageView_condition_icon)
             }
         })
+    }
+
+    private fun chooseLocalizedUnitAbbreviation(metric: String, imperial: String): String {
+         return if (viewModel.isMetric) metric else imperial
+    }
+
+    private fun updateLocation(location: String){
+        (activity as? AppCompatActivity)?.supportActionBar?.title = location
+    }
+
+    private fun updateDateToToday(){
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = "Today"
+    }
+
+    private fun updateTemperatures(temperature: Double, feesLike: Double){
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("°C", "°F")
+        textView_temperature.text = "$temperature$unitAbbreviation"
+        textView_feels_like_temperature.text = "Feels like $feesLike$unitAbbreviation"
+    }
+
+    private fun updateCondition(condition: String){
+        textView_condition.text = condition
+    }
+
+    private fun updatePrecipitation(precipitation: Double){
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("mm","in")
+        textView_precipitation.text = "Precipitation: $precipitation $unitAbbreviation"
+    }
+
+    private fun updateWind(windDirection: String, windSpeed: Double){
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("kph", "mph")
+        textView_wind.text = "Wind: $windDirection, $windSpeed $unitAbbreviation"
+    }
+
+    private fun updateVisibility(visibilityDistance: Double){
+        val unitAbbreviation = chooseLocalizedUnitAbbreviation("km", "mi")
+        textView_visibility.text = "Visibility: $visibilityDistance $unitAbbreviation"
     }
 }
